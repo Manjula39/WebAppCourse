1e93bab542ce2f93adccbf3e09590acfa6905e36
Manjula36 <manjulavdt@gmail.com>
2024-01-31 16:04:23 +0530

    Add project files.

---
+++ WebAppCourse/Controllers/CourseController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppCourse.Models;
+
+namespace WebAppCourse.Controllers
+{
+    public class CourseController : Controller
+    {
+        // GET: Course
+        static List<Course> courses = new List<Course>()
+              {
+        new Course(){Id=1,Name="Manju",Fee=25000,Technology="ASP",CStartDate=new DateTime(day:23,month:06,year:2022),CEndDate=new DateTime(day:30,month:09,year:2022)},
+        new Course(){Id=2,Name="Sanju",Fee=25000,Technology="Javascript",CStartDate=new DateTime(day:23,month:07,year:2022),CEndDate=new DateTime(day:30,month:10,year:2022)},
+        new Course(){Id=3,Name="Swetha",Fee=25000,Technology="python",CStartDate=new DateTime(day:23,month:08,year:2022),CEndDate=new DateTime(day:30,month:11,year:2022)},
+    };
+        public ActionResult Index()
+        {
+            return View(courses);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View(new Course());
+        }
+        [HttpPost]
+        public ActionResult Create(Course C)
+        {
+            if (ModelState.IsValid)
+            {
+                courses.Add(C);
+                return RedirectToAction("Index");
+            }
+            return View(C);
+        }
+    }
+}
+
---
+++ WebAppCourse/Models/Course.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppCourse.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string  Name { get; set; }
+        public double Fee { get; set; }
+        public string Technology { get; set; }
+        public DateTime CStartDate { get; set; }
+        public DateTime CEndDate { get; set; }
+
+
+    }
+}
